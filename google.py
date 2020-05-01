f0b9ad2db9aad9cfd3c51a9172ed1f53646c5a5f
Stigg101 <officialstigg@gmail.com>
2020-05-01 20:36:15 +0200

    init commit

---
+++ google.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome(r"C:/Webdrivers/chromedriver.exe")
+driver.get('https://web.whatsapp.com/')
